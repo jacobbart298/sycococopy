0c3423a40e4a91121f06c93815d529f3e5657c23
Teun <a.h.e.schoutens@live.nl>
2023-09-18 14:55:02 +0200

    Makes a small optimalisation in method getNextState of class State

--- src/core/state.py
+++ src/core/state.py
@@ -9,14 +9,10 @@ class State:
         self.transitionsToState[transition] = state
 
     def getNextState(self, transition): 
-        for key in self.transitionsToState:
-            if key == transition:
-                return self.transitionsToState[key]
+        if transition in self.transitionsToState:
+            return self.transitionsToState[transition]
         return None
     
     def __str__(self) -> str:
         for transition in self.transitionsToState:
             return(f"I'm in a state that needs the transition: {str(transition)}")
-
-
-
