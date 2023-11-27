dba76b1be101fc3b2a857e9b004fc96844f3758f
Jacob Bart <jbart@ziggo.nl>
2023-11-27 19:51:15 +0100

    Added typing

--- src/core/monitor.py
+++ src/core/monitor.py
@@ -36,7 +36,7 @@ class Monitor():
     # Destructor method. Note that a destructor does not raise but silences exceptions.
     def __del__(self):
         if not self.halted:
-            fsmInFinalState = self.fsm.inFinalState()
+            fsmInFinalState: bool = self.fsm.inFinalState()
             lostMessages: list[Transition] = []
             for uncheckedReceives in self.uncheckedReceives.values():
                 lostMessages.extend(uncheckedReceives) 
