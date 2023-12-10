d5aa87728919d832c64b3bc8529af9d1e17a7a16
Jacob Bart <jbart@ziggo.nl>
2023-12-10 11:56:50 +0100

    Started non determinism benchmark with tree_monitor.py

---
+++ benchmarks/non_determinism_benchmarks/tree_monitor.py
@@ -0,0 +1,38 @@
+
+
+def writeSpecification(level: int) -> None:
+    indent = "    "
+    # write role header
+    specification = "roles:\n"
+    # write roles
+    specification += indent + "A\n"
+    specification += indent + "B\n"
+    # write protocol header
+    specification += "\nprotocol:\n"
+    # write sequence expression
+    specification += indent + "choice:\n"
+    specification += writeProtocol(1, 2, level)
+    specification += writeProtocol(1, 2, level)
+
+    with open(r'.\tree_monitor_protocol.txt', 'w') as spec:
+        spec.write(specification)
+
+def writeProtocol(depth: int, indentLevel: int, maxDepth: int) -> str:
+    indent = "    "
+    if depth == maxDepth and depth % 2 == 1:
+        return indentLevel*indent + "send bool from A to B\n"
+    elif depth == maxDepth and depth % 2 == 0:
+        return indentLevel*indent + "send bool from B to A\n"
+    else:
+        protocol = indentLevel*indent + "sequence:\n"
+        indentLevel += 1
+        if depth % 2 == 1:
+            protocol += indentLevel*indent + "send bool from A to B\n"
+        else:
+            protocol += indentLevel*indent + "send bool from B to A\n"
+        protocol += indentLevel*indent + "choice:\n"
+        protocol += writeProtocol(depth + 1, indentLevel + 1, maxDepth)
+        protocol += writeProtocol(depth + 1, indentLevel + 1, maxDepth)
+        return protocol
+
+writeSpecification(5)
