5e759ddbfa380f91574edcfa05fde7092f275bcd
Jacob Bart <jbart@ziggo.nl>
2024-01-08 19:53:45 +0100

    documents state class and removes unused imports and __str__ method

--- src/core/state.py
+++ src/core/state.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 from src.core.transition import Transition
-import typing
 
 '''
-The state module offers the State class that represents a state in a Finite State Machine.
-A state contains a dictionary that maps transitions to states.
+The State class represents a state in a Finite State Machine.
+A state contains a dictionary that maps transitions to new states.
 '''
-
 class State:
 
     def __init__(self):
@@ -29,12 +27,3 @@ class State:
     # Returns a list of possible Transitions in this State.
     def getTransitions(self) -> list[Transition]:
         return list(self.transitionsToStates.keys())
-
-    def __str__(self) -> str:
-        if len(self.transitionsToStates) == 0:
-            return "I'm a state that supports no transitions"
-        else:
-            strValue = "I am a state that supports the following transitions:\n"
-            for transition in self.transitionsToStates:
-                strValue += str(transition) + "\n"
-            return strValue
