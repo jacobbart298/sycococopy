5f842f2eddfa4cf343adab8b7ecc775b70cd0524
Teun <a.h.e.schoutens@live.nl>
2024-01-27 13:30:18 +0100

    Adds typing to monitor code

--- src/core/monitor.py
+++ src/core/monitor.py
@@ -1,6 +1,7 @@
 import sys
 from src.core.fsmBuilder import FsmBuilder
 from src.core.transition import Transition
+from src.core.fsm import FSM
 from src.core.exceptions.illegaltransitionexception import IllegalTransitionException
 from src.core.exceptions.haltedexception import HaltedException
 from src.core.exceptions.sycococopyexception import SycococopyException
@@ -19,11 +20,11 @@ class Monitor():
     # haven't been received.
     def __init__(self, filePath: str, checkCausality = True):
         self.halted: bool = False
-        self.checkCausality = checkCausality
+        self.checkCausality: bool = checkCausality
         self.setExceptionHook()
         self.transitionHistory: list[tuple[Transition, any]] = []
         self.uncheckedReceives: dict[str, Transition] = {}
-        self.fsm = FsmBuilder().buildFsm(filePath)
+        self.fsm: FSM = FsmBuilder().buildFsm(filePath)
 
     # Destructor method. Prints error message to console if there are still messages that haven't been
     # received or if the FSM is not in a final state.
@@ -73,15 +74,15 @@ class Monitor():
             self.uncheckedReceives[transition.getReceiver()] = [transition]
     
     # Method that builds the errorMessage in case the program terminates prematurely.
-    def buildErrorMessage(self, lostMessages, hasTerminated):
+    def buildErrorMessage(self, lostMessages: list[Transition], hasTerminated: bool):
         message: str = "\nUNEXPECTED TERMINATION!\n" 
-        if not hasTerminated:
+        if not hasTerminated and self.transitionHistory:
             message += "The program failed to reach the end of the protocol. Only the following messages were sent:\n"
             count: int = 1
             for transition, item in self.transitionHistory:
                 message += f"\t{count}: send {transition.getType().__name__}({item}) from {transition.getSender()} to {transition.getReceiver()}\n"
                 count += 1
-        if len(lostMessages) > 0:
+        if lostMessages:
             message += "The following messages were lost:\n"
             for transition in lostMessages:
                 message += f"\t{transition.getType().__name__} from {transition.getSender()} to {transition.getReceiver()}\n"
